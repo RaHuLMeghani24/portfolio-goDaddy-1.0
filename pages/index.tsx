1e0546560ebeec0906e866f520460a49d68d0c10
RaHuLMeghani24 <rahulmeghani36@gmail.com>
2023-07-03 14:03:16 -0400

    changes in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,20 @@
-import { GetStaticProps } from "next";
+// import { GetStaticProps } from "next";
 import Head from "next/head";
 import { Experience, PageInfo, Skill, Project, Social } from "../typings";
 // import { fetchPageInfo } from "../utils/fetchPageInfo";
 // import { fetchExperiences } from "../utils/fetchExperience";
 // import { fetchProjects } from "../utils/fetchProjects";
 // import { fetchSkills } from "../utils/fetchSkills";
-import About from "../components/About";
-import WorkExperience from "../components/WorkExperience";
-import Skills from "../components/Skills";
-import Projects from "../components/Projects";
-import ContactMe from "../components/ContactMe";
-import Link from "next/link";
-import { HomeIcon } from "@heroicons/react/24/solid";
-import Image from "next/image";
+
+// import About from "../components/About";
+// import WorkExperience from "../components/WorkExperience";
+// import Skills from "../components/Skills";
+// import Projects from "../components/Projects";
+// import ContactMe from "../components/ContactMe";
+// import Link from "next/link";
+// import { HomeIcon } from "@heroicons/react/24/solid";
+// import Image from "next/image";
+
 // import { Inter } from "next/font/google";
 // import styles from "@/styles/Home.module.css";
 import Header from "@/components/Header";
@@ -42,44 +44,37 @@ const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
       <Head>
         <title>{"Rahul's Portfolio"}</title>
       </Head>
-      <div
+      {/* <div
         className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory
-    overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]]/80"
-      >
-        {/* Header */}
-        <Header socials={socials} />
-
-        {/* Hero */}
-        <section id="hero" className="snap-start">
+      overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]]/80"
+      > */}
+      {/* Header */}
+      {/* <Header socials={socials} /> */}
+      {/* Hero */}
+      {/* <section id="hero" className="snap-start">
           <Hero pageInfo={pageInfo} />
-        </section>
-
-        {/* About */}
-        <section id="about" className="snap-center">
+        </section> */}
+      {/* About */}
+      {/* <section id="about" className="snap-center">
           <About pageInfo={pageInfo} />
-        </section>
-
-        {/* Experiences */}
-        <section id="experience" className="snap-center">
+        </section> */}
+      {/* Experiences */}
+      {/* <section id="experience" className="snap-center">
           <WorkExperience experiences={experiences} />
-        </section>
-
-        {/* Skills */}
-        <section id="skills" className="snap-start">
+        </section> */}
+      {/* Skills */}
+      {/* <section id="skills" className="snap-start">
           <Skills skills={skills} />
-        </section>
-
-        {/* Projects */}
-        <section id="projects" className="snap-start">
+        </section> */}
+      {/* Projects */}
+      {/* <section id="projects" className="snap-start">
           <Projects projects={projects} />
-        </section>
-
-        {/* Contact */}
-        <section id="contact" className="snap-start">
+        </section> */}
+      {/* Contact */}
+      {/* <section id="contact" className="snap-start">
           <ContactMe />
-        </section>
-
-        <Link href="#hero">
+        </section> */}
+      {/* <Link href="#hero">
           <footer className="sticky bottom-5 w-full cursor-pointer">
             <div className="flex items-center justify-center">
               <div className="h-10 w-10 bg-darkGreen/80 rounded-full flex items-center justify-center">
@@ -87,30 +82,30 @@ const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
               </div>
             </div>
           </footer>
-        </Link>
-      </div>
+        </Link> */}
+      {/* </div> */}
+      <h1>Hello Portfolio</h1>
     </>
   );
-  return <h1>Hello World</h1>;
 };
 
 export default Home;
 
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const pageInfo = await fetchPageInfo();
-  const experiences = await fetchExperiences();
-  const skills = await fetchSkills();
-  const projects = await fetchProjects();
-  const socials = await fetchSocials();
+// export const getStaticProps: GetStaticProps<Props> = async () => {
+//   const pageInfo = await fetchPageInfo();
+//   const experiences = await fetchExperiences();
+//   const skills = await fetchSkills();
+//   const projects = await fetchProjects();
+//   const socials = await fetchSocials();
 
-  return {
-    props: {
-      pageInfo,
-      experiences,
-      skills,
-      projects,
-      socials,
-    },
-    revalidate: 10,
-  };
-};
+//   return {
+//     props: {
+//       pageInfo,
+//       experiences,
+//       skills,
+//       projects,
+//       socials,
+//     },
+//     revalidate: 10,
+//   };
+// };
